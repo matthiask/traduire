c6433caedf20c6d07291a577d89df377e1662db4
Matthias Kestenholz <mk@feinheit.ch>
2024-07-12 12:35:27 +0200

    Improve the rsbuild/Django integration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "@rsbuild/core"
 
 /* Django and fablib integration starts here */
 const development = process.env.NODE_ENV === "development"
+const host = process.env.HOST
+const port = process.env.PORT
+const backend = process.env.BACKEND
 const template = "./tmp/template.html"
 
 fs.existsSync("./tmp") || fs.mkdirSync("./tmp")
@@ -21,15 +24,27 @@ const integration = {
     },
   },
   dev: {
-    assetPrefix: true,
-    client: { port: 3000 } /* FIXME hardcoded port number is bad */,
+    assetPrefix: `http://${host}:${port}/rsbuild/`,
     writeToDisk: (path) => /\.html$/.test(path),
   },
   html: {
     template,
     meta: false,
   },
-  server: { printUrls: false },
+  server: {
+    host,
+    port,
+    printUrls: false,
+    proxy: {
+      "/": {
+        target: `http://${host}:${backend}`,
+        bypass(req, res, proxyOptions) {
+          if (req.url.includes("/rsbuild/")) return req.url
+          return null
+        },
+      },
+    },
+  },
 }
 /* Django and fablib integration ends here */
 
